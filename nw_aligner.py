6e32ab7a934ee4c75970a3738436508b7fe6497b
MihaKon <konon.michal@gmail.com>
2024-10-27 18:45:08 +0100

    Add error handling during fasta file read

--- nw_aligner.py
+++ nw_aligner.py
@@ -7,15 +7,15 @@ def setup_argparse():
         description="Needleman-Wunsch alignment of two sequences"
     )
     parser.add_argument(
-        "--seq_file_path_1",
         "-f1",
+        "--seq_file_path_1",
         type=str,
         help="Path to the first sequence in FASTA format",
         required=True,
     )
     parser.add_argument(
-        "--seq_file_path_2",
         "-f2",
+        "--seq_file_path_2",
         type=str,
         help="Path to the second sequence in FASTA format",
         required=True,
@@ -26,15 +26,19 @@ def setup_argparse():
 def parse_fasta(path: str) -> SeqIO.SeqRecord:
     with open(path, "r") as file:
         records = SeqIO.parse(file, "fasta")
-        return records.__next__()
+        if next(records, None) is None:
+            raise ValueError(f"No records found in {path}")
+        record = next(records)
+
+    return record
 
 
 def main():
     parser = setup_argparse()
     args = parser.parse_args()
 
-    seq1 = parse_fasta(args.p1)
-    seq2 = parse_fasta(args.p2)
+    seq1 = parse_fasta(args.seq_file_path_1)
+    seq2 = parse_fasta(args.seq_file_path_2)
 
 
 if __name__ == "__main__":
